ec1c978e95bbb0ba10675319182d840e12da44bf
aseeds10 <amorelseeds@gmail.com>
2024-08-30 02:53:24 -0400

    updated table schema, final ver

--- EmployeeSQL/create-tables_schema.sql
+++ EmployeeSQL/create-tables_schema.sql
@@ -20,44 +20,44 @@ DROP TABLE IF EXISTS titles;
 
 -- Create new table
 CREATE TABLE departments (
-dept_no VARCHAR(20) PRIMARY KEY,
-dept_name VARCHAR(30)
+dept_no VARCHAR(20) NOT NULL PRIMARY KEY,
+dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE titles (
-title_id VARCHAR(40) PRIMARY KEY,
-title VARCHAR(40)
+title_id VARCHAR(40) NOT NULL PRIMARY KEY,
+title VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE employees (
-emp_no int PRIMARY KEY,
-emp_title VARCHAR(40),
+emp_no int NOT NULL PRIMARY KEY,
+emp_title VARCHAR(40) NOT NULL,
 birth_date date,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
 sex CHAR(1),
 hire_date date,
 FOREIGN KEY (emp_title) REFERENCES titles(title_id)
 );
 
 CREATE TABLE dept_emp (
-emp_no int,
-dept_no VARCHAR(20),
+emp_no int NOT NULL,
+dept_no VARCHAR(20) NOT NULL,
 PRIMARY KEY(emp_no,dept_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(20),
-emp_no int,
+dept_no VARCHAR(20) NOT NULL,
+emp_no int NOT NULL,
 PRIMARY KEY(emp_no,dept_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
-emp_no int PRIMARY KEY,
+emp_no int NOT NULL PRIMARY KEY,
 salary int,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
